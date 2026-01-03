d37c3b25aced904f5ba5df900af59e014d3686db
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-01-03 14:17:45 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: http://localhost:3000/api`);
 }
 bootstrap();
