1b5c1795b2625dcfc54945b028018e517e71652e
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-01-03 14:37:31 +0000

    [ADD] Module User

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  getHealth() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+      database: 'MongoDB',
+    };
   }
 }
